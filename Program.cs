a00ea1665a27e4b75769f558404f40154f7339e1
Victor Herrera <herreracontrerasva@gmail.com>
2023-06-16 18:40:02 -0300

    extended navigation

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-using BlazingPizza.Data;
-using BlazingPizza.Services;
-
+using BlazingPizza;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +7,6 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient();
 builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
 builder.Services.AddScoped<OrderState>();
-
 
 var app = builder.Build();
 
@@ -25,6 +22,7 @@ app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
 // Initialize the database
 var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
 using (var scope = scopeFactory.CreateScope())
@@ -36,6 +34,6 @@ using (var scope = scopeFactory.CreateScope())
     }
 }
 
-app.Run();
 
 app.Run();
+
